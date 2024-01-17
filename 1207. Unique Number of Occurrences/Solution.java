f22fc2539169903e67e6191b33284feaece6f2be
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-01-17 22:06:32 +0530

    Problem Solved

---
+++ 1207. Unique Number of Occurrences/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n: arr) map.put(n, map.getOrDefault(n, 0)+1);
+
+        Set<Integer> set = new HashSet<>();
+        Iterator<Integer> keys = map.keySet().iterator();
+        while (keys.hasNext()) {
+            int val = map.get(keys.next());
+            if (set.contains(val)) return false;
+            set.add(val);
+        }
+
+        return true;
+    }
+}
