9afedbcf8c141c6770cecef645ceb91dfce2bda2
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-03-20 14:31:02 +0530

    Beats 100% of Java Users

---
+++ 1669. Merge In Between Linked Lists/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ class Solution {
+
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode head = list1;
+        int rem = b-a+1;
+
+        while (a-- > 1) head = head.next;
+
+        ListNode tail = head;
+        while (rem-- > 0) tail = tail.next;
+
+        ListNode temp = list2;
+        while (temp.next != null) temp = temp.next;
+        
+        temp.next = tail.next;
+        head.next = list2;
+
+        return list1;
+    }
+}
