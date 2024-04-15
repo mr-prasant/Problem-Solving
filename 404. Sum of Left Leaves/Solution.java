03964151a31d54b38ad0e31eb1bd1ad33c41e2ec
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-04-16 00:57:39 +0530

    Beats 100% of Java User

---
+++ 404. Sum of Left Leaves/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+ class Solution {
+
+    public int sum(TreeNode node, boolean isLeft) {
+        if (node == null) return 0;
+
+        if (node.left == null && node.right == null && isLeft)
+        return node.val;
+
+        return sum(node.left, true) + sum(node.right, false);
+    }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sum(root, false);
+    }
+}
